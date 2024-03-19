053c5782a15b55993f66a09e5e6c6b7ad51ae5e7
Aetherinox <aetherinox@proton.me>
2024-03-19 07:37:05 -0700

    feat(opengraph): add favicon fetching

--- mkdocs_link_embeds_plugin/opengraph.py
+++ mkdocs_link_embeds_plugin/opengraph.py
@@ -1,40 +1,43 @@
 import urllib.request
-
 from bs4 import BeautifulSoup
 
 class OpenGraph:
 
-    def __init__(self):
+    def __init__( self ):
         self.html_parser = 'html.parser'
         pass
 
-    def get_page(self, url):
+    def get_page( self, url ):
         response = urllib.request.urlopen(url)
         soup = BeautifulSoup(response,
                              self.html_parser,
                              from_encoding=response.info().get_param('charset'))
         return soup
 
-    def get_og_title(self, soup):
-        if soup.findAll("meta", property="og:title"):
-            return soup.find("meta", property="og:title")["content"]
+    def get_title( self, soup ):
+        if soup.findAll( "meta", property="og:title" ):
+            return soup.find( "meta", property="og:title")["content"]
         else:
             return
 
-    def get_og_description(self, soup):
-        if soup.findAll("meta", property="og:description"):
-            return soup.find("meta", property="og:description")["content"]
+    def get_description( self, soup ):
+        if soup.findAll( "meta", property="og:description" ):
+            return soup.find( "meta", property="og:description")["content"]
         else:
             return
 
-    def get_og_site_name(self, soup):
-        if soup.findAll("meta", property="og:site_name"):
-            return soup.find("meta", property="og:site_name")["content"]
+    def get_site_name( self, soup ):
+        if soup.findAll( "meta", property="og:site_name" ):
+            return soup.find( "meta", property="og:site_name")["content"]
         else:
             return
 
-    def get_og_image(self, soup):
-        if soup.findAll("meta", property="og:image"):
-            return soup.find("meta", property="og:image")["content"]
+    def get_image( self, soup ):
+        if soup.findAll( "meta", property="og:image" ):
+            return soup.find( "meta", property="og:image")["content"]
         else:
             return
+
+    def get_favicon( self, soup ):
+        for item in soup.find_all('link', attrs={'rel': re.compile("^(shortcut icon|icon)$", re.I)}):
+            print(item.get('href'))
