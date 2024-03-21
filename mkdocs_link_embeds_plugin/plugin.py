ee0eaed2919558e2a0e38b77ffe4c009f31bab54
Aetherinox <aetherinox@proton.me>
2024-03-21 12:03:27 -0700

    feat: added accent property
    feat: added accent property
    change: property priority

--- mkdocs_link_embeds_plugin/plugin.py
+++ mkdocs_link_embeds_plugin/plugin.py
@@ -15,8 +15,9 @@ meta_def_name                   = "Untitled"
 meta_def_desc                   = "No description"
 meta_def_image                  = "https://github.com/Aetherinox/mkdocs-link-embeds/assets/118329232/98179e23-ce03-4101-a858-56db0cd0e8f0"
 meta_def_favicon                = "https://github.com/Aetherinox/mkdocs-link-embeds/assets/118329232/b37da9c6-6f17-4c3f-9c94-c346a6f31bfa"
-favicon_def_size                = 25
+meta_def_favicon_size           = 25
 target_def_id                   = "new"
+accent_def                      = "#ffffff1a"
 
 # ------------------------------------------------------------------------------------------------------------------------------------------
 #   Link Embed Plugin
@@ -51,8 +52,9 @@ class LinkEmbedsPlugin( BasePlugin ):
         ( 'image_default',      config_options.Type( str, default=f'{meta_def_image}' ) ),
         ( "favicon_disabled",   config_options.Type( bool, default=False ) ),
         ( 'favicon_default',    config_options.Type( str, default=f'{meta_def_favicon}' ) ),
-        ( 'favicon_size',       config_options.Type( int, default=favicon_def_size ) ),
+        ( 'favicon_size',       config_options.Type( int, default=meta_def_favicon_size ) ),
         ( 'target',             config_options.Type( str, default=f'{target_def_id}' ) ),
+        ( 'accent',             config_options.Type( str, default=f'{accent_def}' ) ),
     )
 
     # ------------------------------------------------------------------------------------------------------------------------------------------
@@ -63,7 +65,8 @@ class LinkEmbedsPlugin( BasePlugin ):
     # ------------------------------------------------------------------------------------------------------------------------------------------
 
     #CBLOCK_PATTERN             = re.compile( r"(?<=\n)\n```\bembed\b(?=[^`]*?\nurl:(?P<url>[^`\n]+))?(?=[^`]*?\nname:(?P<name>[^`\n]+))?(?=[^`]*?\nbanner:(?P<banner>[^`\n]+))?(?=[^`]*?\nfavicon:(?P<favicon>[^`\n]+))?(?=[^`]*?\nfavicon_size:(?P<favicon_size>[^`\n]+))?(?=[^`]*?\nimage:(?P<image>[^`\n]+))?(?=[^`]*?\ndesc:(?P<desc>[^`\n]+))?[^`]*?```" )
-    CBLOCK_PATTERN              = re.compile( r"(?<=\n)\n```\bembed\b(?=[^`]*?\nurl:[\s+](?P<url>[^`\n]+))?(?=[^`]*?\nname:[\s+](?P<name>[^`\n]+))?(?=[^`]*?\nbanner:[\s+](?P<banner>[^`\n]+))?(?=[^`]*?\nfavicon:[\s+](?P<favicon>[^`\n]+))?(?=[^`]*?\nfavicon_size:[\s+](?P<favicon_size>[^`\n]+))?(?=[^`]*?\ntarget:[\s+](?P<target>[^`\n]+))?(?=[^`]*?\nimage:[\s+](?P<image>[^`\n]+))?(?=[^`]*?\ndesc:[\s+](?P<desc>[^`\n]+))?[^`]*?```" )
+    #CBLOCK_PATTERN             = re.compile( r"(?<=\n)\n```\bembed\b(?=[^`]*?\nurl:[\s+](?P<url>[^`\n]+))?(?=[^`]*?\nname:[\s+](?P<name>[^`\n]+))?(?=[^`]*?\nbanner:[\s+](?P<banner>[^`\n]+))?(?=[^`]*?\nfavicon:[\s+](?P<favicon>[^`\n]+))?(?=[^`]*?\nfavicon_size:[\s+](?P<favicon_size>[^`\n]+))?(?=[^`]*?\ntarget:[\s+](?P<target>[^`\n]+))?(?=[^`]*?\nimage:[\s+](?P<image>[^`\n]+))?(?=[^`]*?\ndesc:[\s+](?P<desc>[^`\n]+))?[^`]*?```" ) # replace \s with [^\S\n]++
+    CBLOCK_PATTERN              = re.compile( r"(?<=\n)\n```\bembed\b(?=[^`]*?\nurl:? +(?P<url>[^`\n]*))?(?=[^`]*?\nname:? +(?P<name>[^`\n]*))?(?=[^`]*?\nimage:? +(?P<image>[^`\n]*))?(?=[^`]*?\nfavicon:? +(?P<favicon>[^`\n]*))?(?=[^`]*?\nfavicon_size:? +(?P<favicon_size>[^`\n]*))?(?=[^`]*?\ntarget:? +(?P<target>[^`\n]*))?(?=[^`]*?\naccent:? +(?P<accent>[^`\n]*))?(?=[^`]*?\ndesc:? +(?P<desc>[^`\n]*))?[^`]*?```" )
 
     # ------------------------------------------------------------------------------------------------------------------------------------------
     #   Initialize
@@ -107,21 +110,38 @@ class LinkEmbedsPlugin( BasePlugin ):
 
             if self.config[ 'name_default' ] and self.config[ 'name_default' ] != '':
                 box_name            = self.config[ 'name_default' ]
+            else:
+                box_name            = meta_def_name
 
             if self.config[ 'desc_default' ] and self.config[ 'desc_default' ] != '':
                 box_desc            = self.config[ 'desc_default' ]
+            else:
+                box_desc            = meta_def_desc
 
             if self.config[ 'image_default' ] and self.config[ 'image_default' ] != '':
                 box_image           = self.config[ 'image_default' ]
+            else:
+                box_image           = meta_def_image
 
             if self.config[ 'favicon_default' ] and self.config[ 'favicon_default' ] != '':
                 box_favicon         = self.config[ 'favicon_default' ]
+            else:
+                box_favicon         = meta_def_favicon
 
             if self.config[ 'favicon_size' ] and self.config[ 'favicon_size' ] != '':
                 box_favicon_size    = self.config[ 'favicon_size' ]
+            else:
+                box_favicon_size    = meta_def_favicon_size
 
             if self.config[ 'target' ] and self.config[ 'target' ] != '':
                 box_target          = self.config[ 'target' ]
+            else:
+                box_target          = target_def_id
+
+            if self.config[ 'accent' ] and self.config[ 'accent' ] != '':
+                box_accent          = self.config[ 'accent' ]
+            else:
+                box_accent          = accent_def
 
             # -----------------------------------------------------------------------------------------
             #   pull any user inputs provided
@@ -134,6 +154,7 @@ class LinkEmbedsPlugin( BasePlugin ):
             input_favicon           = self.clean_input( site.group( "favicon" ) )
             input_favicon_size      = self.clean_input( site.group( "favicon_size" ) )
             input_target            = self.clean_input( site.group( "target" ) )
+            input_accent            = self.clean_input( site.group( "accent" ) )
 
             lines                   = input_url.splitlines( )
             html_output             = ""
@@ -153,43 +174,35 @@ class LinkEmbedsPlugin( BasePlugin ):
                 #   fetch metadata for website (if available)
                 # -----------------------------------------------------------------------------------------
 
-                box_name                = self.fetchurl.get_title( soup )
-                box_desc                = self.fetchurl.get_description( soup )
-                box_image               = self.fetchurl.get_image( soup )
-                box_favicon             = self.fetchurl.get_favicon( soup )
+                box_name                = self.fetchurl.get_title( soup, box_name )
+                box_desc                = self.fetchurl.get_description( soup, box_desc, link )
+                box_image               = self.fetchurl.get_image( soup, box_image )
+                box_favicon             = not self.config.get( 'favicon_disabled' ) and self.fetchurl.get_favicon( soup, input_url, box_favicon, input_favicon ) or ""
                 box_favicon_size        = str( self.config[ 'favicon_size' ] )
                 box_target              = str( self.config[ 'target' ] )
 
-                # -----------------------------------------------------------------------------------------
-                #   Normal Template
-                #   check for null values
-                # -----------------------------------------------------------------------------------------
-
-                if box_name is None:
-                    if self.config[ 'name_default' ]:
-                        box_name        = self.config[ 'name_default' ]
-                    else:
-                        box_name        = meta_def_name
-
-                    box_name            = re.sub( "[\"\']", "", box_name )
-
-                if box_desc is None:
-                    if self.config[ 'desc_default' ]:
-                        box_desc        = self.config[ 'desc_default' ]
-                    else:
-                        box_desc        = meta_def_desc
-
-                    box_desc            = re.sub( "[\"\']", "", box_desc )
+                #print( box_desc )
 
                 # -----------------------------------------------------------------------------------------
-                #   Object > Image
+                #   Handle > Input Override > Name
+                # -----------------------------------------------------------------------------------------
+    
+                if input_name:
+                    box_name        = input_name
+
+                # -----------------------------------------------------------------------------------------
+                #   Handle > Input Override > Desc
                 # -----------------------------------------------------------------------------------------
 
-                if box_image is None:
-                    if self.config[ 'image_default' ]:
-                        box_image       = self.config[ 'image_default' ]
-                    else:
-                        box_image       = meta_def_image
+                if input_desc:
+                    box_desc            = input_desc
+
+                # -----------------------------------------------------------------------------------------
+                #   Handle > Input Override > Image
+                # -----------------------------------------------------------------------------------------
+
+                if input_image:
+                    box_image           = input_image
 
                 if ( box_image == 'false' ) or ( box_image is None ) or ( not box_image ) or ( self.config.get( 'image_disabled' ) ):
                     style_image         = "display:none;"
@@ -197,14 +210,11 @@ class LinkEmbedsPlugin( BasePlugin ):
                     style_image         = ""
 
                 # -----------------------------------------------------------------------------------------
-                #   Object > Fav Icon
+                #   Handle > Input Override > Favicon
                 # -----------------------------------------------------------------------------------------
 
-                if box_favicon is None:
-                    if self.config[ 'favicon_default' ]:
-                        box_favicon     = self.config[ 'favicon_default' ]
-                    else:
-                        box_favicon     = meta_def_favicon
+                if input_favicon:
+                    box_favicon         = input_favicon
 
                 if ( box_favicon == 'false' ) or ( box_favicon is None ) or ( not box_favicon ) or ( self.config.get( 'favicon_disabled' ) ):
                     style_favicon       = "display:none; padding-right: 0px;"
@@ -212,28 +222,43 @@ class LinkEmbedsPlugin( BasePlugin ):
                     style_favicon       = f"padding-right: 7px; width: {box_favicon_size}px"
 
                 # -----------------------------------------------------------------------------------------
-                #   Object > Target = None
+                #   Handle > Input Override > Favicon Size
                 # -----------------------------------------------------------------------------------------
 
-                if box_target is None:
-                    if self.config[ 'target' ]:
-                        box_target      = self.config[ 'target' ]
-                    else:
-                        box_target      = meta_def_favicon
+                if input_favicon_size:
+                    box_favicon_size    = input_favicon_size
 
                 # -----------------------------------------------------------------------------------------
-                #   Object > Target = Define Keywords
+                #   Handle > Input Override > Target
                 # -----------------------------------------------------------------------------------------
+
+                if input_target:
+                    box_target          = input_target
 
                 if box_target == "_self" or box_target == "self" or box_target == "same" or box_target == "current":
-                    box_target      = "_self"
+                    box_target          = "_self"
                 elif box_target == "_blank" or box_target == "blank" or box_target == "new" or box_target == "window" or box_target == "open":
-                    box_target      = "_blank"
+                    box_target          = "_blank"
                 else:
-                    box_target      = self.config[ 'target' ]
+                    box_target          = self.config[ 'target' ]
 
                 # -----------------------------------------------------------------------------------------
-                #   build normal template
+                #   Handle > Input Override > Accent
+                # -----------------------------------------------------------------------------------------
+
+                if input_accent:
+                    box_accent      = input_accent
+
+                #   remove # from beginning of string and add our own in case the user supplied it.
+                box_accent          = ( box_accent[1:] if box_accent.startswith( '#' ) else box_accent )
+
+                if ( box_accent == 'false' ) or ( box_accent is None ) or ( not box_accent ):
+                    box_accent       = f"border: 2px solid #{accent_def}"
+                else:
+                    box_accent      = f"border: 2px solid #{box_accent}"
+
+                # -----------------------------------------------------------------------------------------
+                #   build template
                 # -----------------------------------------------------------------------------------------
 
                 html_view           = html_view.replace( "{{ link }}", link )
@@ -244,6 +269,7 @@ class LinkEmbedsPlugin( BasePlugin ):
                 html_view           = html_view.replace( "{{ favicon }}", box_favicon )
                 html_view           = html_view.replace( "{{ favicon-style }}", style_favicon )
                 html_view           = html_view.replace( "{{ target }}", box_target )
+                html_view           = html_view.replace( "{{ accent }}", box_accent )
 
                 html_output         += html_view
 
