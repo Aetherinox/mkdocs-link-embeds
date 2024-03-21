7b6be8c401262445b6a72e533aa1b7b8b34bc043
Aetherinox <aetherinox@proton.me>
2024-03-21 12:02:23 -0700

    change: misc changes (see notes)
    - change: added user agent to keep certain sites from blocking crawling
    - change: added backup metadata sources
    - feat: advanced favicon fetching

--- mkdocs_link_embeds_plugin/fetchurl.py
+++ mkdocs_link_embeds_plugin/fetchurl.py
@@ -1,4 +1,6 @@
+import re
 import urllib.request
+import requests
 
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urljoin
@@ -12,6 +14,7 @@ class FetchURL:
     def __init__( self ):
         self.html_parser    = 'html.parser'
         self.encoding       = "utf-8"
+        self.agent          = { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0' }
         pass
 
     # -----------------------------------------------------------------------------------------
@@ -22,16 +25,18 @@ class FetchURL:
 
     def get_page( self, url ):
         try:
-            response    = urllib.request.urlopen( url )
+            opener              = urllib.request.build_opener( )
+            opener.addheaders   = [('User-agent', 'Mozilla/5.0')]
+            response            = opener.open( url )
         except:
-            response    = ""
+            response            = ""
 
         try:
-            encoding    = response.info( ).get_param( 'charset' )
-            soup        = BeautifulSoup( response, self.html_parser, from_encoding=encoding )
+            encoding            = response.info( ).get_param( 'charset' )
+            soup                = BeautifulSoup( response, self.html_parser, from_encoding=encoding )
         except:
-            soup        = BeautifulSoup( response, self.html_parser )
-            soup.prettify( "latin-1" )
+            soup                = BeautifulSoup( response, self.html_parser )
+            soup.prettify       ( "latin-1" )
 
         return soup
 
@@ -39,48 +44,64 @@ class FetchURL:
     #   Get > Title
     # -----------------------------------------------------------------------------------------
 
-    def get_title( self, soup ):
+    def get_title( self, soup, title ):
         if soup.findAll( "meta", property="og:title" ):
             return soup.find( "meta", property="og:title")["content"]
+        elif soup.findAll( 'title' ):
+            return soup.find(' title' )
         else:
-            return
+            return title
 
     # -----------------------------------------------------------------------------------------
     #   Get > Description
     # -----------------------------------------------------------------------------------------
 
-    def get_description( self, soup ):
+    def get_description( self, soup, desc, url ):
         if soup.findAll( "meta", property="og:description" ):
             return soup.find( "meta", property="og:description")["content"]
+        elif soup.findAll( 'meta', attrs={'name': 'description'} ):
+            asdasd = soup.find( 'meta', attrs={ 'name': 'description' } )
         else:
-            return
+            return desc
 
     # -----------------------------------------------------------------------------------------
     #   Get > Name
     # -----------------------------------------------------------------------------------------
 
-    def get_site_name( self, soup ):
+    def get_site_name( self, soup, name ):
         if soup.findAll( "meta", property="og:site_name" ):
             return soup.find( "meta", property="og:site_name")["content"]
+        elif soup.findAll( 'title' ):
+            return soup.find(' title' )
         else:
-            return
+            return name
 
     # -----------------------------------------------------------------------------------------
     #   Get > Image
     # -----------------------------------------------------------------------------------------
 
-    def get_image( self, soup ):
+    def get_image( self, soup, image ):
         if soup.findAll( "meta", property="og:image" ):
             return soup.find( "meta", property="og:image")["content"]
+        elif soup.findAll( "meta", property="twitter:image" ):
+            return soup.find( "meta", property="twitter:image")["content"]
         else:
-            return
+            return image
 
     # -----------------------------------------------------------------------------------------
     #   Get > Fav Icon
+    #
+    #   this is a very "step-by-step" manner of checking for fav icons. Just because there's
+    #   so many ways that a favicon can be added.
     # -----------------------------------------------------------------------------------------
 
-    def get_favicon( self, soup ):
+    def get_favicon( self, soup, url, favicon, input_favicon ):
+
+        if input_favicon:
+            return input_favicon
+
         lnk_favicon         = soup.find( 'link', rel='icon' ) or soup.find( 'link', rel='shortcut icon' )
+        pattern             = r"^(?P<scheme>[^:\/?#]+):(?:\/\/)?(?:(?:(?P<login>[^:]+)(?::(?P<password>[^@]+)?)?@)?(?P<host>[^@\/?#:]*)(?::(?P<port>\d+)?)?)?(?P<path>[^?#]*)(?:\?(?P<query>[^#]*))?(?:#(?P<fragment>.*))?"
 
         if lnk_favicon:
             url_favicon     = lnk_favicon.get( 'href' )
@@ -89,6 +110,47 @@ class FetchURL:
             if url_parsed.netloc != '':
                 return url_favicon
             else:
-                return
+                match               = re.match( pattern, url )
+
+                url_http            = match.group( "scheme" ) or "https"
+
+                url_host            = ( match.group( "host" )[1:] if match.group( "host" ).startswith( '/' ) else match.group( "host" ) )   #   remove forward slash if 1st char
+                url_host            = url_host.rstrip( '//' ) # remove last slash
+
+                url_path            = ( match.group( "path" )[1:] if match.group( "path" ).startswith( '/' ) else match.group( "path" ) )   #   remove forward slash if 1st char
+                url_path            = url_path.rstrip( '//' ) # remove last slash
+
+                url_icon_local      = ( url_favicon[1:] if url_favicon.startswith('/') else url_favicon )   #   remove forward slash from local icon path if found
+                domain              = url_http + "://" + url_host                            #   https://domain.com
+
+                if domain and url_icon_local:
+                    url_icon_local      = domain + "/" + url_icon_local     # create https://domain.com/ favicon.png
+                    response            = requests.get( url_icon_local, headers=self.agent, timeout=( 1, 1 ) )
+                    bSuccess            = False
+                    
+                    if response.status_code == 404:
+                        bSuccess = False
+                    else:
+                        bSuccess = True
+
+                    if not bSuccess:
+                        domain          = url_http + "://" + url_host + "/" + url_path       # add /path/ to end of domain
+                        url_icon_local  = domain + "/" + url_favicon
+
+                        response        = requests.get( url_icon_local, headers=self.agent, timeout=( 1, 1 ) )
+
+                        if response.status_code == 404:
+                            bSuccess = False
+                        else:
+                            bSuccess = True
+
+                    if bSuccess:
+                        return url_icon_local
+                    else:
+                        return favicon
+
+                    return favicon
+                else:
+                    return favicon
         else:
-            return
+            return favicon
